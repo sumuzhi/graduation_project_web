401232450fc7e1a4698877d61bcbd4fb9f920bfa
su_muzhi <su_muzhi@163.com>
2023-04-30 16:47:39 +0800

    feat: 修改readme.md

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 //axios发送请求的服务器的地址
 // export const BASE_URL = 'http://114.132.247.53/api'
-export const BASE_URL = 'http://localhost:3000/api'
-export const WS_URL = 'ws://localhost:3000'
+export const BASE_URL = 'http://localhost:3001/api'
+export const WS_URL = 'ws://localhost:3001'
